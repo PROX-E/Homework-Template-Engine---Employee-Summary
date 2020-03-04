baf456d6d5560baecdd7fcd64e37824cfe81c058
PROX-E <lichlord18@gmail.com>
2020-03-03 21:37:35 -0500

    working build team function

--- app.js
+++ app.js
@@ -32,3 +32,73 @@ function initalize(){
       }
     ])
   }
+
+  function buildTeam(){
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "role",
+        message:"What type of team member would you like to add?",
+        choices: ["Engineer","Intern","I don't want to add any more team members"]
+      }
+    ]).then((answer)=> {
+      if (answer.role === "Engineer"){
+        return inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: `What is your engineer's name?`
+          },
+          {
+            type: "input",
+            name: "id",
+            message: `What is your engineer's ID?`
+          },
+          {
+            type: "input",
+            name: "email",
+            message: `What is your engineer's email?`
+          },
+          {
+            type: "input",
+            name: "github",
+            message: `What is your engineer's GitHub??`
+          }
+        ]).then((answers)=>{
+          let engineer = new Engineer(answers.name, answers.id, answers.email,answers.github);
+          team.splice(team.length-1,0,engineer.getHTML());
+          buildTeam();
+        })
+      }
+      if (answer.role === "Intern"){
+        return inquirer.prompt([
+          {
+            type: "input",
+            name: "name",
+            message: `What is your intern's name?`
+          },
+          {
+            type: "input",
+            name: "id",
+            message: `What is your intern's ID?`
+          },
+          {
+            type: "input",
+            name: "email",
+            message: `What is your intern's email?`
+          },
+          {
+            type: "input",
+            name: "school",
+            message: `What is your intern's school?`
+          }
+        ]).then((answers)=>{
+          let intern = new Intern(answers.name, answers.id, answers.email,answers.school);
+          team.splice(team.length-1,0,intern.getHTML());
+          buildTeam();
+        })
+      }
+  
+      return printHTML(team);
+    });
+  }
