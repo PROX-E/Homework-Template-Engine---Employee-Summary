e2fb3f3db59f006f06a198ba5b755911c8f2fee2
PROX-E <lichlord18@gmail.com>
2020-03-03 21:35:37 -0500

    initialize function to create a manager

--- app.js
+++ app.js
@@ -7,3 +7,28 @@ const fs = require("fs");
 const open = require("open");
 
 const team = [htmlBlocks.header(), htmlBlocks.footer()];
+
+function initalize(){
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: `What is your manager's name?`
+      },
+      {
+        type: "input",
+        name: "id",
+        message: `What is your manager's ID?`
+      },
+      {
+        type: "input",
+        name: "email",
+        message: `What is your manager's email?`
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: `What is your manager's office number?`
+      }
+    ])
+  }
